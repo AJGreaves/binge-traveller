8fa1b7bc8478e6cf55ad26804f1470ccf9a996cb
Anna Greaves <gilhespy.anna@gmail.com>
2021-03-11 15:53:27 +0100

    Add email js

---
+++ assets/js/email.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+    /**
+     * Adds a submit event listener to #contact-form
+     * Sends the form data to emailjs, adds an alert to the user
+     * for successful message sent, or error.
+     */
+    $('#contact-form').submit(function (event) {
+        event.preventDefault();
+        // "gmail" is service ID, "template_ihdtu89" is the template ID
+        emailjs.sendForm('gmail', 'template_gekjfat', this)
+            .then(function () {
+                // If message sent successfully, alert user and then refresh the page
+                alert("Your message was sent successfully!");
+                location.reload();
+            }, function (error) {
+                // If error with sending message, alert user with error message.
+                alert("Ooops, something went wrong...", error);
+            });
+    });
+};

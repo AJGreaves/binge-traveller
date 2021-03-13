0029bd1c4c3093458b9bfabd84f13917d8afb66a
Anna Greaves <gilhespy.anna@gmail.com>
2021-03-13 20:39:18 +0100

    Update background images and remove scrollWheelZoom on map.

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,5 +1,5 @@
 // Set which part of the world the map loads on, and the zoom level
-let mymap = L.map('map').setView([32.856, 5.545], 2);
+let mymap = L.map('map', {scrollWheelZoom: false}).setView([32.856, 5.545], 2,);
 
 // Builds map with access token
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY29kZWluc3RpdHV0ZSIsImEiOiJja20xdnZ3d2cwM2RnMnBvejhvb2RiNHFpIn0.4b-36pNR0ECgagXmeoYwVg', {
